e83a3a53343f3897e29a52b77db4d03221de040d
Tanushree-jain <tanushreegangwal19@gmail.com>
2025-07-30 14:42:41 +0530

    service , controller , and model file

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task } from './task.model';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller('task')
-export class TaskController {}
+@ApiTags('tasks')
+@Controller('tasks')
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Post()
+  create(@Body() dto: CreateTaskDto): Task {
+    return this.taskService.createTask(dto);
+  }
+
+  @Get()
+  findAll(): Task[] {
+    return this.taskService.getAllTasks();
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): void {
+    this.taskService.deleteTask(Number(id));
+  }
+}
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  private tasks: Task[] = [];
+  private id = 1;
+
+  createTask(dto: CreateTaskDto): Task {
+    const task: Task = {
+      id: this.id++,
+      ...dto,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  deleteTask(id: number): void {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+}
